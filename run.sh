06f0296307d94236027a9a605c1b5540100de5de
yerlan <Yerlan from laptop>
2018-01-16 03:54:51 -0800

    fully functional script for training all models

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+for model in resnet20 resnet32 resnet44 resnet56 resnet110 resnet1202
+do
+    echo "python -u trainer.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model"
+    python -u trainer.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model
+done
