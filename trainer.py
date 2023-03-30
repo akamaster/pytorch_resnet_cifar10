81f756c4104ca76e3df49bb8cea73656e915466d
JonnaMat <matthiesenjonna@gmail.com>
2023-03-30 14:31:46 +0200

    update cifar10 normalization

--- trainer.py
+++ trainer.py
@@ -87,8 +87,8 @@ def main():
 
     cudnn.benchmark = True
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+    normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], #[0.485, 0.456, 0.406],
+                                     std=[0.247, 0.243, 0.261])#[0.229, 0.224, 0.225])
 
     train_loader = torch.utils.data.DataLoader(
         datasets.CIFAR10(root='./data', train=True, transform=transforms.Compose([
